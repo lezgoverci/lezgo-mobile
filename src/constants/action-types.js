e04c688248a317f1ac78d7da7a874f8fc48b9d15
vercillius <vercilliusjrmila.dev@gmail.com>
2019-09-12 19:53:44 +0800

    added action types

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -5,3 +5,42 @@ export const FBLOGIN = {
   LOGGING: 'LOGGING',
   SAVEDATA: 'SAVEDATA'
 }
+
+export const CHAT ={
+  REFRESH: 'REFRESH'
+}
+
+export const CONVERSATION = {
+  ARCHIVE: 'ARCHIVE',
+  REPORT: 'REPORT',
+  DELETE: 'DELETE',
+  REMOVE:'REMOVE',
+  MUTE: 'MUTE',
+  MARKREAD: 'MARKREAD',
+  TURNOFF: 'TURNOFF',
+  START: 'START',
+  SETNAME: 'SETNAME'
+}
+
+export const MEMBERS = {
+  ADD: 'ADD',
+  REMOVE: 'REMOVE',
+  SETADMIN: 'SETADMIN',
+}
+
+export const MESSAGE = {
+  SEND: 'SEND',
+  CANCEL: 'CANCEL',
+  EDIT: 'EDIT',
+  DELETE: 'DELETE',
+}
+
+export const REACTION = {
+  LIKE : 'LIKE',
+  HEART : 'HEART',
+  ANGRY : 'ANGRY',
+  SAD: 'SAD',
+  WOW: 'WOW'
+}
+
+
