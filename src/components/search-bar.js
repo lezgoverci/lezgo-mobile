16924d1dccf9473b0dc8c11da991f4948236b737
vercillius <vercilliusjrmila.dev@gmail.com>
2019-09-12 19:55:16 +0800

    updated search bar ui

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,24 +1,64 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity, TextInput } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {keyword: ""}
+    this.state = { keyword: "" }
   }
 
   render() {
     return (
       <View style={styles.container}>
-      <View style={styles.card}>
-        <Icon name="md-add" style={styles.icon} />
-        <Text  style={styles.searchBox}>Search</Text>
-        <TouchableOpacity onPress={()=>this.props.navigation.openDrawer()}>
-          <Icon name="md-menu" style={styles.icon} />
-        </TouchableOpacity>
-        
-      </View>
+        <View style={styles.card}>
+          <View style={{ height: 55 }}>
+            <View style={styles.searchBar}>
+              <View style={styles.searchIcons}>
+                <Ionicons name="md-add" style={styles.icon} />
+                <TextInput placeholder='Search' style={styles.searchBox}></TextInput>
+                <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+                  <Ionicons name="md-menu" style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+
+        </View>
+
+        <View style={styles.card}>
+          <View style={{ height: 55 }}>
+            <View style={styles.filter}>
+              <View style={styles.filterIcons}>
+                <MaterialIcon name='local-activity' style={styles.icon} />
+                <MaterialIcon name='local-bar' style={styles.icon} />
+                <MaterialIcon name='people' style={styles.icon} />
+                <MaterialIcon name='view-list' style={styles.icon} />
+              </View>
+            </View>
+          </View>
+
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.autoSuggestWrapper}>
+          <View style={styles.autoSuggestItems}>
+            <Text>Palawan</Text>
+          </View>
+          <View style={styles.autoSuggestItems}>
+            <Text>Palawan</Text>
+          </View>
+          <View style={styles.autoSuggestItems}>
+            <Text>Palawan</Text>
+          </View>
+          </View>
+          
+          
+        </View>
+
       </View>
     )
   }
@@ -26,25 +66,33 @@ class SearchBar extends Component {
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical:10,
-    zIndex:999,
+    paddingVertical: 10,
+    zIndex: 999,
 
   },
-  icon:{
-    fontSize: 28, 
-    color: '#5BAEB2', 
+  icon: {
+    fontSize: 28,
+    color: '#5BAEB2',
   },
-  searchBox:{
-    flex:1, 
-    flexDirection:'row', 
-    paddingLeft:10,
+  searchBar: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20
+  },
+  searchBox: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingLeft: 10,
     fontSize: 16
   },
+  searchIcons: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
   card: {
-    height: 55,
     width: Dimensions.get('window').width - 50,
     backgroundColor: 'white',
     shadowColor: "#000",
@@ -58,11 +106,28 @@ const styles = StyleSheet.create({
     elevation: 5,
     marginBottom: 12,
     borderRadius: 5,
-    padding:20,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between'
+
+
   },
+  filter: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: '70%'
+  },
+
+  filterIcons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  autoSuggestWrapper:{
+    width:'90%',
+    alignSelf:'center'
+  },
+  autoSuggestItems:{
+    paddingVertical:10
+  }
 })
 
 export default SearchBar;
